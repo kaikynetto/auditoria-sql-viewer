10c5ff7f1f0504379eb21e8a46142b34eb7eb9ae
Kaiky <kaikydiasnetto21@gmail.com>
2025-07-02 17:46:27 -0300

    cors

--- src/pages/api/tournaments.js
+++ src/pages/api/tournaments.js
@@ -1,6 +1,18 @@
 import { Client } from 'pg';
+import Cors from 'cors';
+import initMiddleware from '../../lib/init-middleware'; // <- Caminho relativo ao arquivo
+
+// Inicializa o middleware de CORS
+const cors = initMiddleware(
+  Cors({
+    methods: ['GET', 'POST', 'OPTIONS'],
+    origin: '*', // ⚠️ em produção troque por seu domínio
+  })
+);
 
 export default async function handler(req, res) {
+  await cors(req, res); // Executa o middleware de CORS
+
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
